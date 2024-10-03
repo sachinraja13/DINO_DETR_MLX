9456ede8ce8eb57d5ec098f2796249574b684a38
Sachin Raja <sachinraja@Sachins-MacBook-Pro-3.local>
2024-10-03 18:21:04 +0530

    Added SWIN backbone used in original DINO

--- models/backbone/swin_transformer/_factory.py
+++ models/backbone/swin_transformer/_factory.py
@@ -98,7 +98,7 @@ def swin_v2_small_patch4_window8(num_classes: int = 1000) -> SwinTransformer:
     )
 
 
-def swin_v2_large_patch4_window7(num_classes: int = 1000) -> SwinTransformer:
+def swin_large_patch4_window7(num_classes: int = 1000) -> SwinTransformer:
     embed_dim = 192
     return SwinTransformer(
         patch_size=[4, 4],
@@ -109,8 +109,6 @@ def swin_v2_large_patch4_window7(num_classes: int = 1000) -> SwinTransformer:
         interim_layer_channels=[embed_dim, 2 *
                                 embed_dim, 4*embed_dim, 8*embed_dim],
         stochastic_depth_prob=0.3,
-        block=SwinTransformerBlockV2,
-        downsample_layer=PatchMergingV2,
         num_classes=num_classes,
     )
 
